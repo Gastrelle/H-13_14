7081486e905e59859e624e9d8e90aac91377b2b5
Gastrelle <premudraavasilisa67@gmail.com>
2025-11-24 21:05:46 +0000

    Add files via upload

---
+++ homework13.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+    
+
+int main() {
+
+    setlocale(LC_CTYPE, "Rus");
+
+    char str[1000];
+
+    printf("Введите строку: ");
+    fgets(str, sizeof(str), stdin);
+
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') str[len - 1] = '\0';
+    printf("Исходная строка: %s\n", str);
+
+
+    int space_flag = 0; 
+    int j = 0;
+    for (int i = 0; str[i] != '\0';  i++) {
+        if (str[i] == ' ') {
+            if (!space_flag) {
+                str[j++] = str[i];
+                space_flag = 1;
+            }
+        }
+        else {
+            str[j++] = str[i];
+            space_flag = 0;
+        }
+    }
+    str[j] = '\0';
+
+    printf("Результат: \"%s\"\n", str);
+
+    return 0;
+
+}
